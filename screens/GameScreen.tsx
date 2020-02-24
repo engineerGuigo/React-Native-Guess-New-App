f454af2aa1fee20ffef945d3df26d0fefad4f80f
Rodrigo Durante <rodrigodurante@Guilhermes-MacBook-Pro.local>
2020-02-23 21:50:31 -0300

    AlignItems

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        alignContent: 'center'
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
